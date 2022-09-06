375e6dd63d19474a58d66402b3bf3d0e9f2ce9c8
alemonacoo <ale.monaco@outlook.com>
2022-09-06 15:56:26 +0200

    creating teamMembersArray (objects array)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("JS OK!");
+
+
+const teamMembersArray = 
+[
+    {name:'Wayne Barnett', role: 'Founder & CEO', picture: 'wayne-barnett-founder-ceo.jpg'},
+    {name:'Angela Caroll', role: 'Office Manager', picture: 'walter-gordon-office-manager.jpg'},
+    {name:'Angela Lopez', role: 'Social Media Manager', picture: 'angela-lopez-social-media-manager.jpg'},
+    {name:'Scott Estrada', role: 'Developer', picture: 'scott-estrada-developer.jpg'},
+    {name:'Barbara Ramos', role: 'Graphic Designer', picture: 'barbara-ramos-graphic-designer.jpg'}
+]
+
+console.log(teamMembersArray);
+
+
+
+
+
+
