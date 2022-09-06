e780633408425ed1467e4a2df0da4f7d9446ab2f
alemonacoo <ale.monaco@outlook.com>
2022-09-06 16:05:06 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -12,6 +12,15 @@ const teamMembersArray =
 
 console.log(teamMembersArray);
 
+const teamContainer = document.getElementById("team-members-container");
+
+for(i=0; i < teamMembersArray.length; i++){
+    console.log('name = ' + teamMembersArray[i].name, 'role = ' + teamMembersArray[i].role, 'picture = ' + teamMembersArray[i].picture);
+    teamContainer.innerHTML += `<h2> ${teamMembersArray[i].role}</h2>
+                                <p> ${teamMembersArray[i].role}</p>
+                                <p> ${teamMembersArray[i].picture} = pic path</p>
+                                `
+}
 
 
 
